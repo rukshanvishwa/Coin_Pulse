e8e9da1890199fd344dc1a9142e9b04cd94a9f43
rukshanvishwa <rukshanprakash123@gmail.com>
2026-01-08 02:08:47 +0530

    feat: Enhance CandlestickChart with period selection and loading state management; update constants for chart configuration

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { PERIOD_BUTTONS } from "@/constants";
-import { useState } from "react";
+import { IChartApi, ISeriesApi } from "lightweight-charts";
+import { useRef, useState } from "react";
 
 const CandlestickChart = ({
   children,
@@ -10,7 +11,29 @@ const CandlestickChart = ({
   height = 360,
   initialPeriod = 'daily'
 }: CandlestickChartProps) => {
-  const [loading, setloading] = useState(false);
+  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartRef = useRef<IChartApi | null>(null);
+  const candleSeriesRef = useRef<ISeriesApi<"Candlestick"> | null>(null);
+
+  const [loading, setLoading] = useState(false);
+  const[period, setPeriod]=useState(initialPeriod);
+  const[ohlcData, setOhlcData]=useState<OHLCData[]>(data??[]);
+
+  const fetchOHLCData = async (selectedPeriod:Period) => {
+    try{
+
+    }catch(e){
+      
+    }
+  }
+    
+
+  const handlePeriodChange = (newPeriod:Period) => {
+    if(newPeriod===period) return;
+    //TODO UPDATE PERIOD
+    setPeriod(newPeriod);
+  };
+
   return (<div id="candlestickChart">
     <div className="chart-header">
       <div className="flex-1">{children}</div>
@@ -19,7 +42,9 @@ const CandlestickChart = ({
         <span className="text-sm mx-2 font-medium
         text-purple-100/50">Period:</span>
         {PERIOD_BUTTONS.map(({ value, label }) => (
-          <button key={value} className="config-button" onClick={() => { }}
+          <button key={value} className={period===value? 'config-button active' : 
+            'config-button'} 
+          onClick={() => handlePeriodChange(value)}
             disabled={loading}>
             {label}
           </button>
@@ -27,6 +52,8 @@ const CandlestickChart = ({
 
       </div>
     </div>
+
+    <div ref={chartContainerRef} className="chart" style={{height}} />
   </div>
   );
 };
--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -20,7 +20,7 @@ const CoinOverview = async () => {
     await fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
             vs_currency: 'usd',
             days: 1,
-            interval: 'hourly',
+            // interval: 'hourly',
             precision: 'full',
         })
 
