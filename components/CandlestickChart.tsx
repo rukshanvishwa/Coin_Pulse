f0bc4ca980fcb96904e09e8b7ab39c8a20d1a7ab
rukshanvishwa <rukshanprakash123@gmail.com>
2026-02-24 14:41:02 +0530

    feat: Convert OHLC data timestamps to seconds and update series data handling in CandlestickChart

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -58,9 +58,14 @@ const CandlestickChart = ({
     ...getChartConfig(height, showTime),
     width: container.clientWidth,
   })
-  const series = chart.addSeries(CandlestickSeries, getCandlestickConfig())
+  const series = chart.addSeries(CandlestickSeries, getCandlestickConfig() 
+  );
 
-  series.setData(convertOHLCData(ohlcData));
+const convertedToSeconds=ohlcData.map(
+  (item)=>[Math.floor(item[0]/1000), item[1], item[2], item[3], item[4]] as OHLCData
+);
+
+  series.setData(convertOHLCData(convertedToSeconds));
   chart.timeScale().fitContent();
 
   chartRef.current=chart;
@@ -81,7 +86,7 @@ const CandlestickChart = ({
     
   }
 
- }, [height])
+ }, [height , period ])
 
 useEffect(()=>{
   if(!candleSeriesRef.current) return;
